fc50741580bb40aadf2053cdcb07d81177c01a6c
mikemouse <kmmyemail@ukr.net>
2023-03-08 16:15:48 +0200

    style: interpolation

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const btn = document.querySelector('.btn');
-let timerId,
-	i = 0;
-	
-function myAnimation(){
-	const elem = document.querySelector('.box');
+
+function animateBox() {
+	const box = document.querySelector('.box');
 	let pos = 0;
-	
-	const id = setInterval(frame, 10);
-	
-	function frame(){
-		if(pos == 300){
+	id = setInterval(frame, 10);
+
+	function frame() {
+		if (pos === 300) {
 			clearInterval(id);
-		}else{
-				pos++;
-				elem.style.top = pos + 'px';
-				elem.style.left = pos + 'px';
+		} else {
+			pos++;
+			box.style.top = `${pos}px`;
+			box.style.left = `${pos}px`;
 		}
 	}
 }
 
-btn.addEventListener('click', myAnimation);
+btn.addEventListener('click', animateBox);
